aae4897d828004044a252ee1b39d3d4ea0105f0a
Ashnita Bali <ashnita01@gmail.com>
2019-01-13 00:10:18 +0000

    use runOutsideAngular zone to prevent cd on setInterval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // the expression is evaluated by CD, compared to previous value
+  // and if there's a change the DOM is updated, then the expression
+  // is evaluated again, if the value is different this time, it gives
+  // the changed after checked error. Date.now() will be different on
+  // the second evaluation from the first.
+
+  // get time() {
+  //   return Date.now();
+  // }
+
+  // Since the second check is done synchronouly, updating the time
+  // asynchronously should cause the expression to not change on the
+  // second check.
+  // ... but setTimeout causes change detection causing an infinite loop
+  // of change detection.
   get time() {
-    return Date.now();
+    return this._time;
+  }
+  _time;
+
+  // constructor() {
+  //   this._time = Date.now();
+  //
+  //   setInterval(() => {
+  //     // updated asynchronously
+  //     this._time = Date.now();
+  //   }, 1);
+  // }
+
+  constructor(zone: NgZone) {
+    this._time = Date.now();
+
+    zone.runOutsideAngular(() => {
+      setInterval(() => {
+        this._time = Date.now();
+      });
+    });
   }
 }
